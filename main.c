009404a3641c092302513de08795a9dc78881441
Bonolo <easytmashabela@gmail.com>
2024-01-20 13:57:27 -0800

    Works on machine

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "monty.h"
-#include <stdlib.h>
-#include <stdio.h>
-
+#include "stdio.h"
+#include "stdlib.h"
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
  * main - Monty code interpreter
  * @argc: number of arguments
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = fgets(&content, &size, file);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
